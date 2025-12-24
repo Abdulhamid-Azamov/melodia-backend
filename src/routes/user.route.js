a16c9f094132015ffa0b646023309756fbcb955a
Navoyami-Sao <azzamovabdulkhamid@gmail.com>
2025-12-24 14:39:28 +0500

    debuging

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -5,18 +5,20 @@ import { validator } from '../middlewares/validation-handle.js'
 import authController from '../controllers/auth.controller.js'
 import userValidation from '../validation/user.validation.js'
 import { authGuard } from '../guards/token.guard.js'
+import { roleGuard } from "../guards/role.guard.js";
+import { Roles } from "../enums/index.js";
 
 const userRoute = Router();
 
 userRoute
-    .post('/signup', authGuard, validator(userValidation.create), controller.signup)
+    .post('/signup', validator(userValidation.create), controller.signup)
     .post('/signin', validator(userValidation.signIn), authController.signIn)
     .post('/otp', validator(userValidation.confirmOTP), authController.confirmOtp)
     .post('/signout', authController.signOut)
     .get('/', authGuard, controller.getAll)
     .get('/:id', authGuard, controller.get)
-    .patch('/:id', authGuard, validator(userValidation.update), controller.update)
-    .delete('/:id', authGuard, controller.remove)
+    .patch('/:id', authGuard, roleGuard(Roles.SUPERADMIN,Roles.ADMIN),validator(userValidation.update), controller.update)
+    .delete('/:id', authGuard, roleGuard(Roles.SUPERADMIN,Roles.ADMIN),controller.remove)
 
 
 export default userRoute;

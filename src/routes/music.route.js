595a73f19dd5dfb3846b7b14b6329aecb13286e0
Inomjon <heylronjrat@gmail.com>
2025-12-24 17:44:11 +0500

    Add route to get a random music entry

--- src/routes/music.route.js
+++ src/routes/music.route.js
@@ -8,6 +8,7 @@ const router = express.Router();
 router
     .post("/", validator(musicValidation.create), musics.create)
     .get("/", musics.getAll)
+    .get("/random", musics.getRandom)
     .get("/:id", musics.get)
     .patch("/:id", validator(musicValidation.update), musics.update)
     .delete('/:id', musics.remove)
